7cf036342345713ad5a595da87ba506db107b6ac
Jacob Allen <jallen1887@gmail.com>
2022-09-03 14:16:24 -0400

    Add styles to page

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 function getComputerChoice() {
     let choiceNum = Math.floor(Math.random()*3);
+
     if (choiceNum === 0) {
         return "rock";
     }
@@ -13,88 +14,100 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
-    //console.log(player);
-    //console.log(computerSelection);
 
     if (player === "rock"){
         if (computerSelection === "rock") {
-            console.log("Rock vs Rock: It's a Tie!");
             return 0;
         } 
         else if (computerSelection === "paper") {
-            console.log("Rock vs Paper: Computer Wins!");
             return -1; 
         }
         else {
-            console.log("Rock vs Scissors: Human Wins!");
             return 1;
         }
     }
     else if (player === "paper"){
         if (computerSelection === "rock") {
-            console.log("Paper vs Rock: Human Wins!");
             return 1;
         }
         else if (computerSelection === "paper") {
-            console.log("Paper vs Paper: It's a Tie!");
             return 0;
         }
         else {
-            console.log("Paper vs Scissors: Computer Wins!");
             return -1;
         }
     }
     else if (player === "scissors"){
         if (computerSelection === "rock") {
-            console.log("Scissors vs Rock: Computer Wins!");
             return -1;
         }
         else if (computerSelection === "paper") {
-            console.log("Scissors vs Paper: Human Wins!");
             return 1;
         }
         else {
-            console.log("Scissors vs Scissors: It's a Tie");
             return 0;
         }
     }
-}
-
-function logTest() {
-    console.log("Clicked Here!");
 }
 
 function chooseRPS(e) {
     const playerSelection = e.currentTarget.id;
     const computerSelection = getComputerChoice();
     let resultText = '';
-    console.log(playerSelection);
+
     const result = playRound(playerSelection,computerSelection);
 
     if (result === 0) {
-        resultText = "It's a Tie!"
+        resultText = 
+            `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}
+             vs ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}: It's a Tie!`;
     }
     else if (result === 1) {
-        resultText = "Human Wins!";
+        resultText = 
+            `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}
+             vs ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}: Player Wins!`;
+
         playerScore++;
     }
     else if (result === -1) {
-        resultText = "Computer Wins!";
+        resultText = 
+            `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}
+             vs ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}: Computer Wins!`;
+
         computerScore++;
     }
 
     document.getElementById('score').textContent = 
-        `${resultText}: ${playerScore} vs ${computerScore}`;
+        `${resultText} ${playerScore} vs ${computerScore}`;
 
     if (playerScore >= 3 || computerScore >= 3) {
+        addVictoryText();
         pauseGame();
     }
 }
 
+function addVictoryText() {
+    const victoryText = document.createElement('p');
+    const scoreBox = document.querySelector("div#score-box");
+    victoryText.classList.add('victory-text');
+
+    if (playerScore >= 3) {
+        victoryText.textContent = `Ultimate Victory!`;
+    }
+    else {
+        victoryText.textContent = `Utter Defeat!`;
+    }
+
+    scoreBox.appendChild(victoryText);
+    
+}
+
 function pauseGame() {
     buttons.forEach(button => button.removeEventListener('click', chooseRPS));
+
     const scoreBox = document.querySelector("div#score-box");
     const resetButton = document.createElement('button');
+
     resetButton.classList.add('reset');
     resetButton.textContent = 'Reset!';
     scoreBox.appendChild(resetButton);
@@ -110,6 +123,8 @@ function resetGame(e) {
     
     const scoreBox = document.querySelector("div#score-box");
     const resetButton = document.querySelector('button.reset');
+    const victoryText = document.querySelector('p.victory-text');
+    scoreBox.removeChild(victoryText);
     scoreBox.removeChild(resetButton);
 
     const buttons = document.querySelectorAll('.game-button');
@@ -118,5 +133,6 @@ function resetGame(e) {
 
 let playerScore = 0;
 let computerScore = 0;
+
 const buttons = document.querySelectorAll('.game-button');
 buttons.forEach(button => button.addEventListener('click', chooseRPS));
